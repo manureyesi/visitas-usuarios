99825ebb1dddbf9b71b2f205145eff10a640ac96
SERVER_CONTABO_FIANDEIRA_02 <manureyesi@outlook.es>
2023-04-13 22:00:26 +0200

    Añadir novos archivos wordpress 2023-04-13_22:00.26+0200

--- includes/opciones.php
+++ includes/opciones.php
@@ -1,6 +1,14 @@
 <?php
 
+	// Iniciar sesion
+	//session_start();
+
 	defined('ABSPATH') or die( "Bye bye" );
+
+	// Mostrar errores
+	//ini_set('display_errors', 1); 
+	//ini_set('display_startup_errors', 1);
+	//error_reporting(E_ALL);
 
 	/*
  	* Nuevo menu de administrador
@@ -23,6 +31,9 @@
 		add_menu_page(VISITAS_NOMBRE, VISITAS_NOMBRE, 'edit_pages', VISITAS_RUTA.'/admin/visitas.php', '', 'dashicons-chart-pie', 10);
 		add_submenu_page(VISITAS_RUTA.'/admin/visitas.php', 'Configuración', 'Configuracion', 'manage_options', VISITAS_RUTA.'/admin/configuracion.php');
 	}
+	
+	// Shorcode insertar visitas
+	add_shortcode('insertar_visitas_web_reservas', 'insertarVisitaswebReservas');
 		
 	function insertarEventoVisita() {
 		
@@ -100,9 +111,100 @@
 		return update_option('visitas_dias_visualizacion', $dia);
 	}
 	
-	function insertarVisita() {
-		echo 'Insertar visita';
-		insertarVisitasUsuarios('wordpress', $_SERVER['REMOTE_ADDR'], 'ES', '', '', '', '', '', '', '');
+	function getBrowser() { 
+	  $u_agent = $_SERVER['HTTP_USER_AGENT'];
+	  $bname = 'Unknown';
+	  $platform = 'Unknown';
+	  $version= "";
+
+	  //First get the platform?
+	  if (preg_match('/linux/i', $u_agent)) {
+		$platform = 'Linux';
+	  }elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
+		$platform = 'Mac';
+	  }elseif (preg_match('/windows|win32/i', $u_agent)) {
+		$platform = 'Windows';
+	  }
+
+	  // Next get the name of the useragent yes seperately and for good reason
+	  if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)){
+		$bname = 'Internet Explorer';
+		$ub = "MSIE";
+	  }elseif(preg_match('/Firefox/i',$u_agent)){
+		$bname = 'Mozilla Firefox';
+		$ub = "Firefox";
+	  }elseif(preg_match('/OPR/i',$u_agent)){
+		$bname = 'Opera';
+		$ub = "Opera";
+	  }elseif(preg_match('/Chrome/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+		$bname = 'Google Chrome';
+		$ub = "Chrome";
+	  }elseif(preg_match('/Safari/i',$u_agent) && !preg_match('/Edge/i',$u_agent)){
+		$bname = 'Apple Safari';
+		$ub = "Safari";
+	  }elseif(preg_match('/Netscape/i',$u_agent)){
+		$bname = 'Netscape';
+		$ub = "Netscape";
+	  }elseif(preg_match('/Edge/i',$u_agent)){
+		$bname = 'Edge';
+		$ub = "Edge";
+	  }elseif(preg_match('/Trident/i',$u_agent)){
+		$bname = 'Internet Explorer';
+		$ub = "MSIE";
+	  }
+
+	  // finally get the correct version number
+	  $known = array('Version', $ub, 'other');
+	  $pattern = '#(?<browser>' . join('|', $known) .
+	')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
+	  if (!preg_match_all($pattern, $u_agent, $matches)) {
+		// we have no matching number just continue
+	  }
+	  // see how many we have
+	  $i = count($matches['browser']);
+	  if ($i != 1) {
+		//we will have two since we are not using 'other' argument yet
+		//see if version is before or after the name
+		if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
+			$version= $matches['version'][0];
+		}else {
+			$version= $matches['version'][1];
+		}
+	  }else {
+		$version= $matches['version'][0];
+	  }
+
+	  // check if we have a number
+	  if ($version==null || $version=="") {$version="?";}
+
+	  return array(
+		'userAgent' => $u_agent,
+		'name'      => $bname,
+		'version'   => $version,
+		'platform'  => $platform,
+		'pattern'    => $pattern,
+		'ub'		=> $ub
+	  );
+	}
+
+	
+	function insertarVisitaswebReservas() {
+		
+		if (!isset($_SESSION["registroIp"])) {
+			$actual_link = "$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+			
+			// Comprobar paguina
+			if (str_contains($actual_link, '/wp-json/wp/v2/pages/6')) {
+				$actual_link = "$_SERVER[HTTP_HOST]/accommodation/casa-manola/";
+			} else if (str_contains($actual_link, 'post=6')) {
+				$actual_link = "$_SERVER[HTTP_HOST]/accommodation/casa-manola/";
+			}
+			
+			$ua=getBrowser();
+			insertarVisitasUsuarios($actual_link, $_SERVER['REMOTE_ADDR'], '', '', '', '', '', $ua['platform'], 'Unknown', $ua['ub']);
+			$_SESSION["registroIp"] = 'true';
+		}
+		
 	}
 	
 ?>
